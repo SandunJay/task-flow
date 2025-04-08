25d4cc7cea9c0c54624bbb2efb9ccefba3870056
sandunjay <sandunjayawardhana2001@gmail.com>
2025-04-08 14:02:33 +0530

    dynamic route error fix

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project-board/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => {
+      return Promise.resolve([
+        { id: '1' },
+        { id: '2' }
+      ]);
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
